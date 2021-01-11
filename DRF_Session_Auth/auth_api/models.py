a834f13bc7a70b139cebb4e7da0d2e801ddd29c5
Satyendra <Satyendra199612@gmail.com>
2021-01-11 20:50:18 +0530

    Added 'name' field to Account model

--- DRF_Session_Auth/auth_api/models.py
+++ DRF_Session_Auth/auth_api/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Account(AbstractUser):
     phone = models.CharField(min_length=1000000000, max_length=9999999999)
+    name = models.CharField(max_length=30)
 
     class Meta:
         db_table = 'Account'
